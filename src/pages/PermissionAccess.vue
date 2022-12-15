e7b7a58b3108a82e462e7d429fb241c85ed17980
riyan-eng <fbriyant11@gmail.com>
2022-12-15 15:17:37 +0700

    add permission access

--- src/pages/PermissionAccess.vue
+++ src/pages/PermissionAccess.vue
@@ -12,13 +12,15 @@
                     </button>
                 </div>
                 <div class="px-1">
-                    <button
+                    <button @click="toggleModalInsertOne"
                         class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 mx-auto transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-4 sm:px-8 py-2 text-xs sm:text-sm">
                         Add
                     </button>
                 </div>
             </div>
         </div>
+        <ModalInsertOne @emtOnSubmitInsertOne="onSubmitInsertOne" @emtCloseModalInsertOne="closeModalInsertOne"
+            :showModalInsertOne="showModalInsertOne" :access="access" :permissions="permissions"/>
         <hr style="border-top: 3px double #8c8b8b" />
         <Vue3EasyDataTable table-class-name="customize-table" buttons-pagination show-index :theme-color="'#f48225'" v-model:items-selected="itemsSelected"
             :headers="headers" :items="items" :rows-items="[10, 25, 50, 100]" :rows-per-page="10">
@@ -46,10 +48,12 @@ import PrivateLayout from '../layouts/PrivateLayout.vue';
 import Vue3EasyDataTable from 'vue3-easy-data-table';
 import axios from 'axios';
 import { TrashIcon, PencilSquareIcon, EyeIcon, XMarkIcon } from "@heroicons/vue/24/outline"
+import ModalInsertOne from '../components/permissionAccess/ModalInsertOne.vue';
+
 
 export default {
     components: {
-        PrivateLayout, TrashIcon, PencilSquareIcon, EyeIcon, Vue3EasyDataTable
+        PrivateLayout, ModalInsertOne, TrashIcon, PencilSquareIcon, EyeIcon, Vue3EasyDataTable
     },
     created() {
         this.findAllPermissionAccess()
@@ -68,7 +72,11 @@ export default {
             items: [],
             itemsSelected: [],
             item: {},
-            btnDeleteMany: true
+            btnDeleteMany: true,
+            showModalInsertOne: false,
+            showModalDeleteOne: false,
+            permissions: [],
+            access:[]
         }
     },
     methods: {
@@ -79,7 +87,42 @@ export default {
                 }
             })
             this.items = data.data
-        }
+        },
+        async findAllPermission() {
+            const { data } = await axios.get("/manajemen/permission/findAll", {
+                headers: {
+                    "Authorization": `Bearer ${this.$store.getters.user.accessToken}`
+                }
+            })
+            this.permissions = data.data
+        },
+        async findAllAccess() {
+            const { data } = await axios.get("/manajemen/access/findAll", {
+                headers: {
+                    "Authorization": `Bearer ${this.$store.getters.user.accessToken}`
+                }
+            })
+            this.access = data.data
+        },
+        toggleModalInsertOne() {
+            this.findAllAccess()
+            this.findAllPermission()
+            this.showModalInsertOne = !this.showModalInsertOne;
+        },
+        onSubmitInsertOne(payload) {
+            // this.insertOneRolePermission(payload)
+            console.log(payload)
+        },
+        closeModalInsertOne() {
+            this.showModalInsertOne = !this.showModalInsertOne
+        },
+        onDeleteOne(id) {
+            console.log(id)
+            // this.deleteOnePermission(id)
+        },
+        closeModalDeleteOne() {
+            this.showModalDeleteOne = !this.showModalDeleteOne
+        },
     }
 }
 </script>
